1464ed8bb11cffc5e5838c8524351dc011ec78c5
Bence Daniel Szøke <54317680+rull3211@users.noreply.github.com>
2025-08-19 21:40:12 +0200

    fix: syncronize first and last element clone with actual

--- src/components/BrouchureCarousel.tsx
+++ src/components/BrouchureCarousel.tsx
@@ -3,13 +3,17 @@ import styles from "../styles/brochureCarousel.module.scss"; // See SCSS below
 import BrochurePage from "./BrochurePage";
 import { usePlantsFromCSV } from "../hooks/usePlantsFromCSV";
 import { usePlantCarousel } from "../hooks/usePlantCarusel";
+import { useRef } from "react";
 
 const BrochureCarousel = () => {
   const { plants } = usePlantsFromCSV(
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEv0zZITBKWwg8VJkPpq9BYjujLpAWoX7baPb2eLqE0ta84COtdESHdot5mJs0NwGBoPKAtXCJ5VZF/pub?output=csv"
   );
   const {handleNext, handlePrev,  transitionSpeed, handleTransitionEnd, isTransitioning, position} =  usePlantCarousel(plants.length)
-
+  const firstRef = useRef<HTMLDivElement | null>(null);
+  const firstRefClone = useRef<HTMLDivElement | null>(null);
+  const lastRef = useRef<HTMLDivElement | null>(null);
+  const lastRefClone = useRef<HTMLDivElement | null>(null);
   return (
     <div className={styles.bodyWrapper}>
       <div className={styles.buttons}>
@@ -29,19 +33,29 @@ const BrochureCarousel = () => {
           onTransitionEnd={handleTransitionEnd}
         >
           {/* Clone last slide */}
-          <div className={styles.slide}>
+          <div ref={lastRefClone} className={styles.slide}>
             <BrochurePage plant={plants[plants.length-1]} />
           </div>
 
           {/* Real slides */}
-          {plants.map((plant, i) => (
-            <div className={styles.slide} key={i}>
+          {plants.map((plant, i) => {
+            let refToUse = null
+            if(i === 0)refToUse = firstRef
+            else if(i === plants.length -1) refToUse = lastRef
+            return(
+            
+            <div  className={styles.slide} ref={refToUse} onScroll={
+              (e)=>{
+                const scrollTop = e.currentTarget.scrollTop;
+                if(i === 0 && firstRefClone.current != null)firstRefClone.current.scrollTop= scrollTop
+                else if(i === plants.length -1 && lastRefClone.current != null)lastRefClone.current.scrollTop= scrollTop
+            }} key={i}>
               <BrochurePage plant={plant} />
             </div>
-          ))}
+          )})}
 
           {/* Clone first slide */}
-          <div className={styles.slide}>
+          <div ref={firstRefClone}  className={styles.slide}>
             <BrochurePage plant={plants[0]} />
           </div>
         </div>
